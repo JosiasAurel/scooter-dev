5a7aced8417fbe491349d4e94c01b8999ab3d469
JosiasAurel <ndjosiasaurel@gmail.com>
2021-01-31 15:35:06 +0100

    make page for auth wait

---
+++ pages/auth.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Auth() {
+	return (
+		<>
+		<h1 style={{textAlign: "center", fontFamily: "monospace"}}>
+		Authentication Coming soon ;P
+		</h1>
+		</>
+	)
+}
+
+export default Auth
---
+++ pages/index.js
@@ -0,0 +1,70 @@
+import React from "react"
+import styles from "../styles/index.module.css"
+import Image from "next/image"
+import Link from "next/link"
+
+function Index() {
+	return (
+		<>
+		<header className={styles.header}>
+		 <Image
+		  src="/scooterLogo.svg"
+		  width="30px"
+		  height="30px" />
+		 <h2> Scooter </h2>
+		</header>
+
+		<div>
+			<span className={styles.logoContainer}>
+			<Image
+			src="/scooterLogo.svg"
+			width="200px"
+			height="200px"
+			/>
+			</span>
+			<span className={styles.startButtonContainer}> 
+			<Link href="/auth">
+			<button className={styles.getStartedButton}>
+				Get Started
+			 </button>
+			</Link>
+			</span>
+		</div>
+
+		<div className={styles.c}>
+		<div className={styles.features}>
+		<span className={styles.card}>
+			<p>
+				Social Media Preview
+			</p>
+		</span>
+		<span className={styles.card}>
+			<p>
+				Website Embed
+			</p>
+		</span>
+		<span className={styles.card}>
+			<p>
+				Syntax Highlight
+			</p>
+		</span>
+		<span className={styles.card}>
+			<p>
+				Minimal & Fast
+			</p>
+		</span>
+
+		</div>
+		</div>
+
+		<footer className={styles.footer}>
+		<p> Built by 	<a href="http://josiasaurel.tech">
+		 Josias Aurel
+		</a>
+		</p>
+		</footer>
+		</>
+	)
+}
+
+export default Index
