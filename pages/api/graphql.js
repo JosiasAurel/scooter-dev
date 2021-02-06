c9eca934f96a5ade8931b9427f90ba52f261a414
JosiasAurel <ndjosiasaurel@gmail.com>
2021-02-06 18:22:14 +0100

    use apollo server micro for vercel

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,4 +1,5 @@
-const { ApolloServer, gql } = require("apollo-server")
+const { ApolloServer, gql } = require("apollo-server-micro")
+
 const mongoose = require('mongoose')
 
 const ScootSchema = new mongoose.Schema({
