cbaf4748ee4ce973592fc25a6c0a64f0b95ae837
JosiasAurel <ndjosiasaurel@gmail.com>
2021-02-06 18:27:12 +0100

    change graphql server uri

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
 import React from "react"
 
 const client = new ApolloClient({
-	uri: "http://localhost:4000",
+	uri: "/api/graphql",
 	cache: new InMemoryCache()
 })
 
