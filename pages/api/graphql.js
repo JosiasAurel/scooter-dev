669e0718bb8e8f150bcba17f1aca21d08dec94c1
Josias Aurel <ndjosiasaurel@gmail.com>
2021-02-06 16:20:01 +0100

    update exported graphql function

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -3,11 +3,9 @@ const { ApolloServer, gql } = require("apollo-server-micro")
 const { typeDefs } = require("./typeDefs")
 const { resolvers } = require("./resolvers")
 
-const server = new ApolloServer({playground: true, typeDefs, resolvers})
+const server = new ApolloServer({ typeDefs, resolvers });
 
-export default server.createHandler({
-	path: '/api/graphql'
-})
+module.exports = server.createHandler({path: "/api/graphql/"});
 
 export const config = {
 	api: {
