8565364e1f8c844ad7b687f8ea52975c825d2d51
Josias Aurel <ndjosiasaurel@gmail.com>
2021-02-06 16:37:00 +0100

    Update graphql.js

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -3,15 +3,17 @@ const { ApolloServer, gql } = require("apollo-server-micro")
 const { typeDefs } = require("./typeDefs")
 const { resolvers } = require("./resolvers")
 
-const server = new ApolloServer({ typeDefs, resolvers });
 
-module.exports = server.createHandler({path: "/api/graphql"});
 
 export const config = {
 	api: {
 		bodyParser: false
 	}
 }
+
+const server = new ApolloServer({playground: true, typeDefs, resolvers });
+
+module.exports = server.createHandler({path: "/api/graphql"});
 
 /*
 server.listen().then(({url}) => {
