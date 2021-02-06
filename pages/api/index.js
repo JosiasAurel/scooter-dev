52cf90a46064334cd12a746c6b51c256144b4827
JosiasAurel <ndjosiasaurel@gmail.com>
2021-02-06 15:48:47 +0100

    export graphql api handler

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,10 +1,16 @@
-const { ApolloServer, gql } = require("apollo-server")
+const { ApolloServer, gql } = require("apollo-server-micro")
 
 const { typeDefs } = require("./typeDefs")
 const { resolvers } = require("./resolvers")
 
 const server = new ApolloServer({playground: true, typeDefs, resolvers})
 
+export default server.createHandler({
+	path: '/api'
+})
+
+/*
 server.listen().then(({url}) => {
 	console.log(`[working]: ${url}`)
 })
+*/
