663ff562f0719029ee2235af6f77617d79d2e0be
JosiasAurel <ndjosiasaurel@gmail.com>
2021-02-06 18:03:53 +0100

    remove external files

--- pages/api/config.js
+++
@@ -1,6 +0,0 @@
-
-const MONGO_DB_URI = "mongodb+srv://scooter:scooter500@cluster0.7kwvr.mongodb.net/scooter?retryWrites=true&w=majority"
-
-const DB_NAME = "scooter"
-
-module.exports = { MONGO_DB_URI, DB_NAME }
